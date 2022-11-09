fb23affe6174c72307f678572aaf3381e63aacb1
Klaus Kazlauskas <klausmachado@gmail.com>
2022-11-08 21:31:14 -0500

    feat(zustand-with-extras): add profile stats

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 // import App from './NormalContext'
 // import App from './SimplerContext'
 // import App from './SimplerContextWithExtras'
-import App from './Zustand'
-// import App from './ZustandWithExtras'
+// import App from './Zustand'
+import App from './ZustandWithExtras'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
