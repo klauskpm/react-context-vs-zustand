817a9f454ae67956462bc5ab8287bf1a8f18f859
Klaus Kazlauskas <klausmachado@gmail.com>
2024-01-06 17:29:35 -0500

    feat: create tracking table

--- src/components/Tracker.jsx
+++ src/components/Tracker.jsx
@@ -1,18 +1,50 @@
 import './Tracker.css';
 
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {storedRenderCounts, resetLogs} from "../Performance/renderCount";
 
 export const Tracker = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [, setTimer] = useState(0);
 
   const openDialog = () => setIsOpen(true);
   const closeDialog = () => setIsOpen(false);
+  const modules = Object.keys(storedRenderCounts);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(timer => timer + 1);
+    }, 100);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className='tracker'>
       {isOpen && (
         <div className='dialog'>
           <p>Tracker</p>
+          {/* renders table from the storedRenderCounts */}
+          <table>
+            <thead>
+            <tr>
+              <th>Module / Component</th>
+              <th>Trigger Count</th>
+              <th>Finished Count</th>
+            </tr>
+            </thead>
+            <tbody>
+            {modules.map((module) => (
+              Object.keys(storedRenderCounts[module]).map((component) => (
+                <tr key={module + " / " + component}>
+                  <td>{module + " / " + component}</td>
+                  <td>{storedRenderCounts[module][component].triggered}</td>
+                  <td>{storedRenderCounts[module][component].finished}</td>
+                </tr>
+              ))
+            ))}
+            </tbody>
+          </table>
+          <button onClick={resetLogs}>Reset tracking</button>
           <button onClick={closeDialog}>
             Close Tracker
           </button>
