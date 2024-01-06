9489ebffcad321a2b71b91179b970c63db0745b4
Klaus Kazlauskas <klausmachado@gmail.com>
2024-01-06 17:11:27 -0500

    feat: create space to show tracking measure

--- src/components/Tracker.jsx
+++ src/components/Tracker.jsx
@@ -1,21 +1,26 @@
-import {useState} from "react";
+import './Tracker.css';
 
+import {useState} from "react";
 
 export const Tracker = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleOpen = () => setIsOpen(!isOpen);
+  const openDialog = () => setIsOpen(true);
+  const closeDialog = () => setIsOpen(false);
 
   return (
-    <>
-      <button onClick={toggleOpen}>
-        {isOpen ? "Close" : "Open"}
-      </button>
+    <div className='tracker'>
       {isOpen && (
-        <div>
-          <p>Content</p>
+        <div className='dialog'>
+          <p>Tracker</p>
+          <button onClick={closeDialog}>
+            Close Tracker
+          </button>
         </div>
       )}
-    </>
+      <button onClick={openDialog}>
+        Open Tracker
+      </button>
+    </div>
   )
 }
